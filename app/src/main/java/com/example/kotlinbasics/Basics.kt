d3f7b5eca09dd7037c70d69d877d27b6bdfeb5d3
argir <kindred0310@gmail.com>
2022-11-01 12:58:36 +0200

    Learning Kotlin- Member Variables

--- app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,24 +1,45 @@
 package com.example.kotlinbasics
 
+import android.app.Person
+
 fun main(){
     //σχόλιο
     //TODO:add new memes here
+    var p1 = MobilePhone("Android","Samsung",2020)
+    var p2 = MobilePhone("iOS","Apple",2022, 5)
+    p1.stateHobby()
+    p2.hobby = "playing soccer"
+    p2.stateHobby()
+}
 
-    var roomTemp = 10
-    var feltTemp = "Cold"
+fun myFunction(a:Int){
+    var a = a
+}
 
-    while(feltTemp == "Cold"){
-         roomTemp++
-        if(roomTemp >= 20){
-            feltTemp = "Comfy"
-            println("Its Comfy now")
-        }
+class MobilePhone constructor(osName: String, brand: String, year: Int){
+
+    var age: Int? = null
+    var hobby: String = "Watching netflix"
+    var osName: String? = null
+
+
+
+
+
+    constructor(osName: String, brand: String, year: Int, age: Int): this(osName,brand,year){
+        this.age = age
+        println("Initializing phone: $osName, $brand, $year, $age")
+
     }
-    println("finished")
 
+    init {
+        println("Initializing phone: $osName, $brand, $year, $age")
+        this.osName = osName
+    }
 
-
-
+    fun stateHobby (){
+        println("$osName hobby is: $hobby")
+    }
 
 }
 
